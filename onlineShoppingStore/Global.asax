467b48503bf17bf9f0f89d427f822761627400bc
coderNet2021 <cnss.saida@gmail.com>
2021-08-17 12:19:40 +0300

    Add project files.

---
+++ onlineShoppingStore/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="onlineShoppingStore.MvcApplication" Language="C#" %>
---
+++ onlineShoppingStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(onlineShoppingStore.Startup))]
+namespace onlineShoppingStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
